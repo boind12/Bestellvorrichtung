f514effd7603086a5eb27c09f8688b66ec3b177c
boind12 <boind12@gmail.com>
2016-11-15 09:00:57 +0100

    Create pkw.cs

---
+++ AutoFahren/pkw.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_fahren
+{
+    class PKW : Automobil
+    {
+        public int personenzahl = 5;
+        public Boolean kombi = false;
+    }
+}
